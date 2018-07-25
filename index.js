1bb760a8833fc4835c64b68c0fd30a9d6362b255
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 21:56:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function nestedTarget(){
   return document.getElementById("nested").querySelector("div.target")
 }
 
-function increaseRank
+function increaseRankBy(givenNumber){
+  var classes = document.querySelector("")
+}
