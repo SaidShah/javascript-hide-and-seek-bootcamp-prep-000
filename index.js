82fb8d14c87c1160759d5218eb71d1a259840f1d
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 21:55:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,4 @@ function nestedTarget(){
   return document.getElementById("nested").querySelector("div.target")
 }
 
-function deepestChild(){
- var queryS = document.querySelector("#grand-node")
- var queryM = queryS.querySelectorAll("div")
- for(let i =0;i<queryM.length;i++){
-   if(queryM[i].innerHTML==="boo"){
-     return queryM[i]
-   }
- }
-  
-}
+function increaseRank
