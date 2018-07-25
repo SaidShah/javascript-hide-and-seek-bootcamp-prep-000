818ba66b90c10c4ba75093fdf5a639e15fdce0ba
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 22:03:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function increaseRankBy(givenNumber){
   var classes = document.querySelector("ul.ranked-list")
   var numbers = classes.querySelectorAll("li")
   for(let i =0;i<numbers.length;i++){
-    numbers[i].innerHTML =  (parseInt(numbers[i].innerHTML)+givenNumber).toString()
+    numbers[i].innerHTML =  (parseInt(numbers[i+1].innerHTML)+givenNumber).toString()
   }
 }
