2e5362b14e8dc870c94beda6ecaf4ff3fb802e4a
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 23:53:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelector("div.target")
+}
+
+function increaseRankBy(givenNumber){
+ var list = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+ 
+ for(let i =0;i<list.length;i++){
+   list[i].innerHTML = (parseInt(list[i].innerHTML)+givenNumber)
+ }
+ 
+}
+
+function deepestChild(){
+
+var lastNode = "#grand-node"
+var start = document.querySelector("div#grand-node").querySelectorAll("div")
+for(let i = 0;i<start.length;i++){
+  lastNode+=" div"
+}
+
+ return document.querySelector(lastNode)
+}
+
