d763649b13b454510332afc6b9ed1b8d146153a3
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 21:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nestedTarget(){
 }
 
 function deepestChild(){
- const list = document.querySelector("div#grand-node").querySelector("div")[4].innerHTML
+ const list = document.querySelector("div#grand-node")
   return list.innerHTML
 }
