de6f8d64e6e9a6b859783fc6816e297793890065
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 22:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function increaseRankBy(givenNumber){
   for(let i =0;i<numbers.length;i++){
     var num = parseInt(numbers[i].innerHTML)
     num+=givenNumber
-    numbers[i].innerHTML =  num.toString()
+    numbers[i].innerHTML =  num
   }
 }
