666b701d954864efb123a80ec6bc7cece69afc44
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-25 22:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(givenNumber){
   var classes = document.querySelector("ul.ranked-list")
   var numbers = classes.querySelectorAll("li")
   for(let i =0;i<numbers.length;i++){
-   var num = (parseInt(numbers[i].innerHTML)+givenNumber)
-    numbers[i] = num
+  (parseInt(numbers[i].innerHTML)+givenNumber)
+    numbers[i] = 
   }
 }
